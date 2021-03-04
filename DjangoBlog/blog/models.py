581839d93600607c4828fd5a13e2e161b68a87b9
Nuctermeron <76808832+Nuctermeron@users.noreply.github.com>
2021-03-04 20:01:49 +0100

    added views, urls path, post viewer, pagination

--- DjangoBlog/blog/models.py
+++ DjangoBlog/blog/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOCIES = (
         ('draft', 'Draft'),
@@ -17,6 +23,12 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOCIES, default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year, self.publish.strftime('%m'), self.publish.strftime('%d'), self.slug])
 
 
 class Meta:
